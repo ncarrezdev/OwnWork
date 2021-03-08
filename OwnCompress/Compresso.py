54a9d880b2af0e0f2a079c89acdf7760f227eec2
NCarrezDev <n.carrez.dev@gmail.com>
2021-03-08 08:37:40 +0000

    Ajout licence manquante

--- OwnCompress/Compresso.py
+++ OwnCompress/Compresso.py
@@ -1,3 +1,10 @@
+'''
+This project and its content is under proprietary licence and belongs to Nicolas CARREZ (n.carrez.dev@gmail.com).
+Copyright (c) Nicolas CARREZ 
+
+The code in this repository is NOT free for use.
+Please read the LICENCE file before making any use of the code below
+'''
 from NewEve import check_time
 
 def map_char_quantity(string_to_map):
